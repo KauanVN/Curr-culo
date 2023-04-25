50e2b3135904a85ec235947f9747997787eae804
KauedeSouza <114521349+KauedeSouza@users.noreply.github.com>
2023-04-25 10:48:55 -0300

    fudeu

---
+++ Curriculo/script.js
@@ -0,0 +1,44 @@
+let tags = ['h1',
+'h3',
+'div',
+'img',
+'p']
+let listaTags = []
+let body = document.querySelector('body')
+
+// criei as 3 tags: div, img e p
+tags.forEach(tag => {
+    let tagNova = criarTags(tag)
+    listaTags.push(tagNova)
+});
+
+listaTags[0].textContent = "Kaue de Souza"
+listaTags[1].textContent = "PUBLICITÁRIO"
+
+// adicionei a div no DOM
+body.appendChild(listaTags[2])
+listaTags[0].classList.add('title')
+listaTags[1].classList.add('title2')
+
+// incluindo na div a tag img e a p
+
+listaTags[2].classList.add('texto')
+
+listaTags[2].appendChild(listaTags[0])
+listaTags[2].appendChild(listaTags[1])
+
+
+
+/*
+let body = document.querySelector('body')
+let p = 
+p.textContent = "Everson Bacelli"
+body.appendChild(p)
+*/
+
+
+// método fabrica
+function criarTags(tag){
+    let novaTag = document.createElement(tag)
+    return novaTag
+}
